97acf53459e351313928180325c06482e561fb52
Jordan Franklin <jordo189@gmail.com>
2021-07-29 16:35:19 -0400

    runs on different port than front end

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express, { Express, Request, Response } from 'express';
 
 
 
-const PORT = 3000;
+const PORT = 3001;
 const app: Express = express();
 
 
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from the TypeScript world! Jordan</h1>');
+  res.send('<h1>Hello from the TypeScript world!</h1>');
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
