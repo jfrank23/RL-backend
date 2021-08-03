e6352e434907ad06c499b9dde750450b638abee6
Jordan Franklin <jordo189@gmail.com>
2021-08-03 18:04:22 -0400

    added postgress startup stuff

--- readme.md
+++ readme.md
@@ -4,3 +4,16 @@ npm install
 npm run serve
 
 It should automatically restart the server when you change index.ts
+
+Database setup:
+Ensure you have an env.json file in the RL-Backend/ folder
+{
+	"user": "postgres",
+	"host": "localhost",
+	"database": "rldata",
+	"password": "YOUR PASSWORD HERE!",
+	"port": 5432
+}
+
+psql --username postgres
+create database rldata;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from 'express';
-
-
+import pg from "pg";
 
 const PORT = 3001;
 const app: Express = express();
 
+const env = require("../env.json");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+pool.connect().then(function () {
+  console.log(`Connected to database ${env.database}`);
+});
 
 
 app.get('/', (req: Request, res: Response) => {
